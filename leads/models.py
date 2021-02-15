a33d0ebc5f3519ad1aa457db2501018b788dac43
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-02-16 00:58:47 +0530

    Database deleted and created again.

--- leads/models.py
+++ leads/models.py
@@ -12,19 +12,19 @@ class Agent(models.Model):
 
 class Lead(models.Model):
 
-    SOURCE_CHOICES = (
-        ('youtube', 'YouTube'),
-        ('google', 'Google'),
-        ('newsletter', 'NewsLetter')
-    )
+    # SOURCE_CHOICES = (
+    #     ('youtube', 'YouTube'),
+    #     ('google', 'Google'),
+    #     ('newsletter', 'NewsLetter')
+    # )
 
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     age = models.IntegerField(default=0)
-
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOICES, max_length=50)
-
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_files = models.FileField(blank=True, null=True)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    # phoned = models.BooleanField(default=False)
+    # source = models.CharField(choices=SOURCE_CHOICES, max_length=50)
+
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField(blank=True, null=True)
