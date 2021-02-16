82c973b86b0ac02d3d16b228a5ed1a19e5b50c1b
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-02-16 11:45:49 +0530

    basic setup

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import leads_list, lead_detail
+from .views import leads_list, lead_detail, lead_create
 
 app_name = "leads"
 
 urlpatterns = [
     path('', leads_list),
-    path('<pk>/', lead_detail),
+    path('<int:pk>/', lead_detail),
+    path('create/', lead_create),
 ]
