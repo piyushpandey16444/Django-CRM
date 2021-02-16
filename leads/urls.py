12d7de369ffc84df4088389c7ba15c6b8d253fd4
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-02-16 13:50:27 +0530

    scripts added, urls updated

--- leads/urls.py
+++ leads/urls.py
@@ -4,9 +4,9 @@ from .views import leads_list, lead_detail, lead_create, lead_update, lead_delet
 app_name = "leads"
 
 urlpatterns = [
-    path('', leads_list),
-    path('<int:pk>/', lead_detail),
-    path('<int:pk>/update/', lead_update),
-    path('<int:pk>/delete/', lead_delete),
-    path('create/', lead_create),
+    path('', leads_list, name="lead-list"),
+    path('<int:pk>/', lead_detail, name="lead-detail"),
+    path('<int:pk>/update/', lead_update, name="lead-update"),
+    path('<int:pk>/delete/', lead_delete, name="lead-delete"),
+    path('create/', lead_create, name="lead-create"),
 ]
