fab87010dd771d078c63bfeafd762976b4bd0857
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-02-16 10:52:03 +0530

    first views and admin registeration.

--- leads/models.py
+++ leads/models.py
@@ -9,22 +9,15 @@ class User(AbstractUser):
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
+
 
 class Lead(models.Model):
-
-    # SOURCE_CHOICES = (
-    #     ('youtube', 'YouTube'),
-    #     ('google', 'Google'),
-    #     ('newsletter', 'NewsLetter')
-    # )
-
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
 
-    # phoned = models.BooleanField(default=False)
-    # source = models.CharField(choices=SOURCE_CHOICES, max_length=50)
-
-    # profile_picture = models.ImageField(blank=True, null=True)
-    # special_files = models.FileField(blank=True, null=True)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
