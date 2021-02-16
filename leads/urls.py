e214a4159cc18c8a5c229e2038f06c5aafa8c4de
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-02-16 15:15:26 +0530

    changed to CBV

--- leads/urls.py
+++ leads/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from .views import leads_list, lead_detail, lead_create, lead_update, lead_delete
+from .views import (
+    leads_list, lead_detail, lead_create, lead_update, lead_delete, LeadListView, LeadDetailView,
+    LeadCreateView,
+)
 
 app_name = "leads"
 
 urlpatterns = [
-    path('', leads_list, name="lead-list"),
-    path('<int:pk>/', lead_detail, name="lead-detail"),
+    path('', LeadListView.as_view(), name="lead-list"),
+    # path('', leads_list, name="lead-list"),
+    path('<int:pk>/', LeadDetailView.as_view(), name="lead-detail"),
+    # path('<int:pk>/', lead_detail, name="lead-detail"),
     path('<int:pk>/update/', lead_update, name="lead-update"),
     path('<int:pk>/delete/', lead_delete, name="lead-delete"),
-    path('create/', lead_create, name="lead-create"),
+    path('create/', LeadCreateView.as_view(), name="lead-create"),
+    # path('create/', lead_create, name="lead-create"),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse, JsonResponse
 from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
+from django.views.generic import ListView, TemplateView, DetailView, CreateView
+
+
+class LandingPageView(TemplateView):
+    template_name = "leads/landing.html"
 
 
 def landing_page(request):
     return render(request, 'leads/landing.html')
+
+
+class LeadListView(ListView):
+    template_name = "leads/lead_list.html"
+    queryset = Lead.objects.all()
+    context_object_name = "leads"
 
 
 def leads_list(request):
@@ -16,10 +27,25 @@ def leads_list(request):
     return render(request, "leads/lead_list.html", context)
 
 
+class LeadDetailView(DetailView):
+    template_name = "leads/lead_detail.html"
+    queryset = Lead.objects.all()
+    context_object_name = "lead"
+
+
 def lead_detail(request, pk):
     lead = Lead.objects.get(pk=pk)
     context = {"lead": lead}
     return render(request, 'leads/lead_detail.html', context)
+
+
+class LeadCreateView(CreateView):
+    template_name = "leads/lead_create.html"
+    form_class = LeadModelForm
+    context_object_name = "lead"
+
+    def get_success_url(self):
+        return reverse("leads:lead-list")
 
 
 def lead_create(request):
