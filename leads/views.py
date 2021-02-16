1606e2f0f5fd0cbb4294028dd1c626a0e278bbcb
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-02-16 11:05:01 +0530

    starting basic setup

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 
 def home_view(request):
-    return render(request, 'leads/home_page.html')
+    leads = Lead.objects.all()
+    context = {
+        'name': "Joe",
+        'age': 20,
+        'leads': leads,
+    }
+    return render(request, "leads/home_page.html", context)
