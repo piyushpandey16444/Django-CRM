c1a923b3d780c4e008ab16788643bc808dea113d
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-02-16 12:49:51 +0530

    update and delete lead functionality

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import Lead, Agent
-from .forms import LeadForm
+from .forms import LeadForm, LeadModelForm
 
 
 def leads_list(request):
@@ -16,20 +16,35 @@ def leads_list(request):
 
 def lead_detail(request, pk):
     lead = Lead.objects.get(pk=pk)
-    context = {
-        "lead": lead,
-    }
+    context = {"lead": lead}
     return render(request, 'leads/lead_detail.html', context)
 
 
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == "POST":
-        form = LeadForm(data=request.POST)
+        form = LeadModelForm(data=request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data.get('first_name', False)
-            last_name = form.cleaned_data.get('last_name', False)
-            age = form.cleaned_data.get('age', False)
-            agent = Agent.objects.first()
+            form.save()
+            return redirect('/leads')
     context = {"form": form}
     return render(request, 'leads/lead_create.html', context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(pk=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(data=request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+
+    context = {"form": form, "lead": lead}
+    return render(request, "leads/lead_update.html", context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(pk=pk)
+    lead.delete()
+    return redirect("/leads")
